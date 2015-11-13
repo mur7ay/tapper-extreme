f7a62caeb6a63b99f562f12b60b5aa73842f9a0e
mur7ay <mur7ay@me.com>
2015-11-13 11:29:15 -0500

    added files for tapper extreme

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -9,17 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // variables
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    
+    // outlets 
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var tabLabel: UILabel!
+    
+    @IBAction func onCoinTapped(sender: UIButton!) {
+        
+        currentTaps++
+        updateTapsLabel()
+        
+        if isGameOver() {
+            restartGame()
+        }
     }
+    
+    @IBAction func initialDisplay(sender: UIButton!) {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        if textField.text != nil && textField.text != "" {
+            
+            logoImageView.hidden = true
+            textField.hidden = true
+            playButton.hidden = true
+            
+            tapButton.hidden = false
+            tabLabel.hidden = false
+            
+            maxTaps = Int(textField.text!)!
+            currentTaps = 0
+            
+            updateTapsLabel()
+        }
     }
-
+    
+    func restartGame() {
+        maxTaps = 0
+        textField.text = ""
+        
+        logoImageView.hidden = false
+        textField.hidden = false
+        playButton.hidden = false
+        
+        tabLabel.hidden = true
+        tapButton.hidden = true
+    }
+    
+    func isGameOver()->Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func updateTapsLabel() {
+        tabLabel.text = "\(currentTaps) taps"
+    }
 
 }
 
